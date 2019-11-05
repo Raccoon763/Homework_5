317a347f3ee08f75300d75c8f7aab0e8a8b03890
Raccoon763 <56101337+Raccoon763@users.noreply.github.com>
2019-11-05 21:14:52 +0300

    Add files via upload

---
+++ Консольный файловый менеджер.py
@@ -0,0 +1,50 @@
+import victory
+import schet
+
+print('Взгляните на меню:')
+print('1. Cоздать папку')
+print('2. Удалить файл/папку')
+print('3. Копировать файл/папку')
+print('4. Просмотр содержимого рабочей директории')
+print('5. Посмотреть только папки')
+print('6. Посмотреть только файлы')
+print('7. Просмотр информации об операционной системе')
+print('8. Создатель программы')
+print('9. Играть в викторину')
+print('10. Мой банковский счет')
+print('11. Выход')
+
+while True:
+    choice = input('Выберите один из пунктов меню:')
+    if choice == '1':
+        first.create_dir()
+
+    elif choice == '2':
+        first.remove_dir()
+
+    elif choice == '3':
+        first.copy_dir()
+
+    elif choice == '4':
+        first.show_dir()
+
+    elif choice == '5':
+        first.show_dir_pap()
+
+    elif choice == '6':
+        first.show_dir_file()
+
+    elif choice == '7':
+        first.os_inf()
+
+    elif choice == '8':
+        first.me()
+
+    elif choice == '9':
+        victory.victory()
+
+    elif choice == '10':
+        schet.schet()
+
+    elif choice == '11':
+        break
